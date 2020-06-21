a480bac9f9e03d94acda4a2bec45cee6754b5843
Emranul Hakim <emranul.hakim@jjay.cuny.edu>
2020-06-21 13:56:44 -0400

    Addin a new line

--- MyCollection/src/main/java/com/osa/list/MyList.java
+++ MyCollection/src/main/java/com/osa/list/MyList.java
@@ -3,6 +3,8 @@ package com.osa.list;
 public class MyList {
 	public static void main(String[] args) {
 		System.out.println("This is the project for my List");
+		
+		System.out.println("This is the project for my List");
 	}
 
 }
